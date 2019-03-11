038ab54be64e775d6b9b8e95c5c2ba285ca6c20b
Max Podufalov <max.podufalov27@gmail.com>
2019-03-11 20:05:40 +0200

    1

--- gzhel/js/common.js
+++ gzhel/js/common.js
@@ -1,17 +1,16 @@
-$(document).ready(function($) {
-    
-    $('.parallax-window').parallax({ imageSrc: "https://maxpodufalov.github.io/gzhel/img/banner.jpg"});
-
-	$(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
+$(document).ready(function ($) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 
 });
