384b44488af7859cf6467062c0e36c63da0c933f
Max Podufalov <max.podufalov27@gmail.com>
2019-03-25 14:55:29 +0200

    Update common.js

--- newagetest/js/common.js
+++ newagetest/js/common.js
@@ -5,29 +5,21 @@ $(function () {
     $("#loadMore").on("click", function (e) {
         e.preventDefault();
         $(".item:hidden")
-            .slice(0, "item:hidden".length)
+            .slice(0, 8)
             .slideDown();
 
         if ($("item:hidden").length == 0) {
             $("#loadMore").fadeOut("slow");
         }
-
-        $("html,body").animate({
-                scrollTop: $(this).offset().top
-            },
-            1500
-        );
     });
 
 });
 
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
-
-
